e601329f9aa2b010536b2f26f939e113d7d65635
Eden Yehualashet <edenyehualashet27@gmail.com>
2025-06-29 02:32:35 -0700

    Add simple BankAccount class project

--- temp_conversion_tool.py
+++ temp_conversion_tool.py
@@ -1,3 +1,4 @@
+# Global conversion factors
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
 
@@ -7,10 +8,9 @@ def convert_to_celsius(fahrenheit):
 def convert_to_fahrenheit(celsius):
     return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
 
-def temperature_converter():
+def main():
     try:
-        temp_input = input("Enter the temperature to convert: ")
-        temperature = float(temp_input)
+        temperature = float(input("Enter the temperature to convert: "))
         unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").strip().upper()
 
         if unit == "F":
@@ -20,9 +20,9 @@ def temperature_converter():
             fahrenheit = convert_to_fahrenheit(temperature)
             print(f"{temperature}°C is {fahrenheit}°F")
         else:
-            raise ValueError("Invalid unit.")
+            print("Invalid unit. Please enter 'C' or 'F'.")
     except ValueError:
         print("Invalid temperature. Please enter a numeric value.")
 
-temperature_converter()
-
+if __name__ == "__main__":
+    main()
